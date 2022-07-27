dc8786c5443b256d4b2eeeafb149a96ae63b2fa3
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2022-07-27 21:54:02 +0100

    Updates test function with exception handling/defensive programming

--- inflammation/models.py
+++ inflammation/models.py
@@ -53,12 +53,12 @@ def patient_normalise(data):
     Negative values are rounded to 0.
     """
 
+    if np.any(data < 0):
+        raise ValueError("Inflammation values should not be negative")
+
     max_data = np.nanmax(data, axis=1)
     with np.errstate(invalid="ignore", divide="ignore"):
         normalised = data / max_data[:, np.newaxis]
     normalised[np.isnan(normalised)] = 0
     normalised[normalised < 0] = 0
     return normalised
-
-
-...
